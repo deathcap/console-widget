255558d0f8d65059d32f7e975e426dbd302f9ba0
deathcap <deathcap@gmx.co.uk>
2016-03-31 21:40:32 -0700

    Convert to native ES6, remove CoffeeScript

--- console-widget.js
+++ console-widget.js
@@ -1,174 +1,202 @@
+'use strict';
 
-EventEmitter = (require 'events').EventEmitter
-vkey = require 'vkey'
+const EventEmitter = require('events').EventEmitter;
+const vkey = require('vkey');
 
-MAX_LINES = 999
+const MAX_LINES = 999;
 
-class ConsoleWidget extends EventEmitter
-  constructor: (@opts) ->
-    @opts ?= {}
-    @opts.widthPx ?= 500
-    @opts.rows ?= 10
-    @opts.lineHeightPx ?= 20
-    @opts.font ?= '12pt Menlo, Courier, \'Courier New\', monospace'
-    @opts.backgroundImage ?= 'linear-gradient(rgba(0,0,0,0.6) 0%, rgba(0,0,0,0.6) 100%)'
-    @opts.closeKeys ?= ['<escape>']
-    @opts.hideTimeout ?= 5000 # ms
+class ConsoleWidget extends EventEmitter {
+  constructor(opts) {
+    super();
 
-    @history = []
-    @historyCursor = @history.length
+    this.opts = opts; 
+    if (!this.opts) this.opts = {};
+    if (this.opts.widthPx === undefined) this.opts.widthPx = 500;
+    if (this.opts.rows === undefined) this.opts.rows = 10;
+    if (this.opts.lineHeightPx === undefined) this.opts.lineHeightPx = 20;
+    if (this.opts.font === undefined) this.opts.font = '12pt Menlo, Courier, \'Courier New\', monospace';
+    if (this.opts.backgoundImage === undefined) this.opts.backgroundImage = 'linear-gradient(rgba(0,0,0,0.6) 0%, rgba(0,0,0,0.6) 100%)';
+    if (this.opts.closeKeys === undefined) this.opts.closeKeys = ['<escape>'];
+    if (this.opts.hideTimeout === undefined) this.opts.hideTimeout = 5000; // ms
 
-    @hideTimer = undefined
+    this.history = [];
+    this.historyCursor = this.history.length;
 
-    @createNodes()
+    this.hideTimer = undefined;
 
-  show: () ->
-    @containerNode.style.visibility = ''
-    @containerNode.style.transition = ''
-    @containerNode.style.opacity = 1.0
-    clearTimeout @hideTimer if @hideTimer?
+    this.createNodes();
+  }
 
-  hide: () ->
-    @containerNode.style.visibility = 'hidden'
+  show() {
+    this.containerNode.style.visibility = '';
+    this.containerNode.style.transition = '';
+    this.containerNode.style.opacity = 1.0;
+    if (this.hideTimer) {
+      clearTimeout(this.hideTimer);
+    }
+  }
 
-  open: (text=undefined) ->
-    @show()
-    @setInput(text) if text?
-    @registerEvents()
-    @focusInput()
+  hide() {
+    this.containerNode.style.visibility = 'hidden';
+  }
 
-  close: () ->
-    @unregisterEvents()
-    @hide()
+  open(text) {
+    this.show();
+    if (text !== undefined) {
+      this.setInput(text);
+    }
+    this.registerEvents();
+    this.focusInput();
+  }
 
-  isOpen: () ->
-    @isShown()
+  close() {
+    this.unregisterEvents();
+    this.hide();
+  }
 
-  isShown: () ->
-    @containerNode.style.visibility != 'hidden' and @containerNode.style.opacity|0 != 0
+  isOpen() {
+    return this.isShown();
+  }
 
-  log: (text) ->
-    @logNode(document.createTextNode(text))
+  isShown() {
+    return this.containerNode.style.visibility !== 'hidden' && this.containerNode.style.opacity|0 !== 0;
+  }
 
-  logNode: (node) ->
-    if not @isShown()
-      # if logged without shown, show then hide after a time interval
-      @show()
-      @hideTimer = setTimeout @fadeOut.bind(@), @opts.hideTimeout
+  log(text) {
+    this.logNode(document.createTextNode(text));
+  }
 
-    @outputNode.appendChild(node)
-    @outputNode.appendChild(document.createElement('br'))
-    @scrollOutput()
-    # TODO: discard last lines
+  logNode(node) {
+    if (!this.isShown()) {
+      // if logged without shown, show then hide after a time interval
+      this.show();
+      this.hideTimer = setTimeout(this.fadeOut.bind(this), this.opts.hideTimeout);
+    }
 
-  fadeOut: () ->
-    @containerNode.style.transition = 'opacity linear 1s' # TODO: add/remove CSS class instead of adding/removing style directly
-    @containerNode.style.opacity = 0.0
+    this.outputNode.appendChild(node);
+    this.outputNode.appendChild(document.createElement('br'));
+    this.scrollOutput();
+    // TODO: discard last lines
+  }
+
+  fadeOut() {
+    this.containerNode.style.transition = 'opacity linear 1s'; // TODO: add/remove CSS class instead of adding/removing style directly
+    this.containerNode.style.opacity = 0.0;
+  }
  
-  focusInput: () ->
-    @inputNode.focus()
+  focusInput() {
+    this.inputNode.focus();
+  }
 
-  setInput: (text) ->
-    @inputNode.value = text
-    @inputNode.setSelectionRange(text.length, text.length)
+  setInput(text) {
+    this.inputNode.value = text;
+    this.inputNode.setSelectionRange(text.length, text.length);
+  }
 
-  scrollOutput: () ->
-    #@outputNode.scrollByLines?(MAX_LINES + 1) # nonstandard
-    # Scroll to very end (scrollHeight will be greater than scrollTop)
-    @outputNode.scrollTop = @outputNode.scrollHeight
+  scrollOutput() {
+    //this.outputNode.scrollByLines?(MAX_LINES + 1); // nonstandard
+    // Scroll to very end (scrollHeight will be greater than scrollTop)
+    this.outputNode.scrollTop = this.outputNode.scrollHeight;
+  }
 
-  createNodes: () ->
-    @containerNode = document.createElement('div')
-    @containerNode.setAttribute 'style', "
-    width: #{@opts.widthPx}px;
-    height: #{@opts.lineHeightPx * @opts.rows}px;
+  createNodes() {
+    this.containerNode = document.createElement('div');
+    this.containerNode.setAttribute('style', `
+    width: ${this.opts.widthPx}px;
+    height: ${this.opts.lineHeightPx * this.opts.rows}px;
     border: 1px solid white;
     color: white;
     visibility: hidden;
     bottom: 0px;
     position: absolute;
-    font: #{@opts.font};
-    background-image: #{@opts.backgroundImage};
-    "
+    font: ${this.opts.font};
+    background-image: ${this.opts.backgroundImage};
+    `);
 
-    @outputNode = document.createElement('div')
-    @outputNode.setAttribute 'style', "
+    this.outputNode = document.createElement('div');
+    this.outputNode.setAttribute('style', `
     overflow-y: scroll; 
     width: 100%;
-    height: #{@opts.lineHeightPx * (@opts.rows - 1)}px;
-    "
-    # TODO: scrollbar styles for better visibility 
+    height: ${this.opts.lineHeightPx * (this.opts.rows - 1)}px;
+    `);
+    // TODO: scrollbar styles for better visibility 
 
-    @inputNode = document.createElement('input')
-    @inputNode.setAttribute 'style', "
+    this.inputNode = document.createElement('input');
+    this.inputNode.setAttribute('style', `
     width: 100%;
-    height: #{@opts.lineHeightPx}px;
+    height: ${this.opts.lineHeightPx}px;
     padding: 0px;
     border: 1px dashed white;
     background-color: transparent;
     color: white;
-    font: #{@opts.font};
-    "
+    font: ${this.opts.font};
+    `);
 
-    @containerNode.appendChild(@outputNode)
-    @containerNode.appendChild(@inputNode)
+    this.containerNode.appendChild(this.outputNode);
+    this.containerNode.appendChild(this.inputNode);
 
-    document.body.appendChild(@containerNode)  # note: starts off hidden
+    document.body.appendChild(this.containerNode);  // note: starts off hidden
+  }
 
-  registerEvents: () ->
-    document.body.addEventListener 'keydown', @onKeydown = (ev) =>
-      key = vkey[ev.keyCode]
+  registerEvents() {
+    document.body.addEventListener('keydown', this.onKeydown = (ev) => {
+      const key = vkey[ev.keyCode];
 
-      preventDefault = true
+      let preventDefault = true;
 
-      if key == '<enter>'
-        return if @inputNode.value.length == 0
+      if (key === '<enter>') {
+        if (this.inputNode.value.length === 0) return;
 
-        @history.push @inputNode.value
-        @historyCursor = @history.length - 1
-        @emit 'input', @inputNode.value
-        @inputNode.value = ''
-      else if key == '<up>'
-        if ev.shiftKey
-          @outputNode.scrollByLines?(-1)
-        else
-          @inputNode.value = @history[@historyCursor] if @history[@historyCursor]?
-          @historyCursor -= 1
-          @historyCursor = 0 if @historyCursor < 0
-      else if key == '<down>'
-        if ev.shiftKey
-          @outputNode.scrollByLines?(1)
-        else
-          @inputNode.value = @history[@historyCursor] if @history[@historyCursor]?
-          @historyCursor += 1
-          @historyCursor = @history.length - 1 if @historyCursor > @history.length - 1
-      else if key == '<page-up>'
-        if ev.shiftKey
-          @outputNode.scrollByLines?(-1)
-        else if ev.ctrlKey or ev.metaKey
-          @outputNode.scrollByLines?(-MAX_LINES)
-        else
-          @outputNode.scrollByPages?(-1)
-      else if key == '<page-down>'
-        if ev.shiftKey
-          @outputNode.scrollByLines?(1)
-        else if ev.ctrlKey or ev.metaKey
-          @outputNode.scrollByLines?(MAX_LINES)
-        else
-          @outputNode.scrollByPages?(1)
-      else if @opts.closeKeys.indexOf(key) != -1
-        @close()
-      else
-        # let unrecognized keys pass through
-        preventDefault = false
+        this.history.push(this.inputNode.value);
+        this.historyCursor = this.history.length - 1;
+        this.emit('input', this.inputNode.value);
+        this.inputNode.value = '';
+      } else if (key === '<up>') {
+        if (ev.shiftKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(-1);
+        } else {
+          if (this.history[this.historyCursor] !== undefined) this.inputNode.value = this.history[this.historyCursor];
+          this.historyCursor -= 1;
+          if (this.historyCursor < 0) this.historyCursor = 0;
+        }
+      } else if (key === '<down>') {
+        if (ev.shiftKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(1);
+        } else {
+          if (this.history[this.historyCursor] !== undefined) this.inputNode.value = this.history[this.historyCursor];
+          this.historyCursor += 1;
+          if (this.historyCursor > this.history.length - 1) this.historyCursor = this.history.length - 1;
+        }
+      } else if (key === '<page-up>') {
+        if (ev.shiftKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(-1);
+        } else if (ev.ctrlKey || ev.metaKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(-MAX_LINES);
+        } else {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByPages(-1);
+        }
+      } else if (key === '<page-down>') {
+        if (ev.shiftKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(1);
+        } else if (ev.ctrlKey || ev.metaKey) {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByLines(MAX_LINES);
+        } else {
+          if (this.outputNode.scrollByLines) this.outputNode.scrollByPages(1);
+        }
+      } else if (this.opts.closeKeys.indexOf(key) !== -1) {
+        this.close();
+      } else {
+        // let unrecognized keys pass through
+        preventDefault = false;
+      }
 
-      ev.preventDefault() if preventDefault
+      if (preventDefault) ev.preventDefault();
+    });
+  }
 
-  unregisterEvents: () ->
-    document.body.removeEventListener 'keydown', @onKeydown
+  unregisterEvents() {
+    document.body.removeEventListener('keydown', this.onKeydown);
+  }
+}
 
-module.exports = (opts) ->
-  new ConsoleWidget(opts)
-
-
-
+module.exports = (opts) => new ConsoleWidget(opts);
